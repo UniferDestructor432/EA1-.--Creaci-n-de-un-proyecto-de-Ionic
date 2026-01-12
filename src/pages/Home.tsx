5b7f1550d4101bab158edca29da80d0b6b608cbe
alumno 4204 <alprepa@cedi.site>
2026-01-12 14:49:54 -0600

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -7,16 +7,67 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Perfil</IonTitle>
         </IonToolbar>
       </IonHeader>
+
+      {/*<IonContent fullscreen>
+        <div id="container">
+          <h1>Mi Perfil</h1>
+          <div className='fileContent'>
+            <strong>Nombre: </strong>
+            <input type='text' className='Text' placeholder='Tu nombre aquí'></input>
+          </div>
+          
+          <div className='fileContent'>
+            <strong>Grupo: </strong>
+            <input type='text' className='Text' placeholder='Tu grupo aquí'></input>
+          </div>
+          
+          <div className='fileContent'>
+            <strong>Materia: </strong>
+            <input type='text' className='Text' placeholder='Tu materia aquí'></input>
+          </div>
+          
+          <div className='fileContent'>
+            <strong>Correo: </strong>
+            <input type='email' className='Text' placeholder='tucorreo@ejemplo.com'></input>
+          </div>
+          
+          <div className='fileContent'>
+            <strong>Descripción:</strong>
+            <input type='text' className='Text' placeholder='Esta es una breve descripción sobre mí y mi primer proyecto en React'></input>
+          </div>
+        </div>
+      </IonContent>*/}
+
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <h1>Mi Perfil</h1>
+        <IonItem> {/* Elemento tipo fila */}
+          <IonLabel> {/* Texto que se muestra dentro de un IonItem */}
+            <strong>Nombre: </strong><span>Johan</span>
+          </IonLabel>
+        </IonItem>
+        <IonItem> {/* Elemento tipo fila */}
+          <IonLabel> {/* Texto que se muestra dentro de un IonItem */}
+            <strong>Grupo: </strong><span>6°C</span>
+          </IonLabel>
+        </IonItem>
+        <IonItem> {/* Elemento tipo fila */}
+          <IonLabel> {/* Texto que se muestra dentro de un IonItem */}
+            <strong>Materia: </strong><span>DAMM</span>
+          </IonLabel>
+        </IonItem>
+        <IonItem> {/* Elemento tipo fila */}
+          <IonLabel> {/* Texto que se muestra dentro de un IonItem */}
+            <strong>Correo: </strong><span>14125@portalcedi.edu.mx</span>
+          </IonLabel>
+        </IonItem>
+        <IonItem> {/* Elemento tipo fila */}
+          <IonLabel> {/* Texto que se muestra dentro de un IonItem */}
+            <strong>Descripción: </strong><span>Como parte de esta materia de creación de apps, he aprendido el día de hoy acerca del uso de los labels ION como IonContent, IonLabel, IonHeader, o IonPage para estructurar una app.</span>
+          </IonLabel>
+        </IonItem>
       </IonContent>
     </IonPage>
   );
