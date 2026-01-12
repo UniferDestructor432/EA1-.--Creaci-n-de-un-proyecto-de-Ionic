7ccde53df9c49dc39c8d80d6f6fcb026c899df88
alumno 4204 <alprepa@cedi.site>
2026-01-12 12:23:22 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EA1_myapp',
+  webDir: 'dist'
+};
+
+export default config;
